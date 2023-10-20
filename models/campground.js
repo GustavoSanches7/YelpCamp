95965c40022f580f55f525fca8287edb9890cead
Nitro747 <gustavo.sanches.silva@gmail.com>
2023-10-20 20:32:01 -0300

    added uploading of image to Mongo Server, needs to fix bootstrap's columns

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
-    title: String, 
-    image: String,
+    title: String,
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
@@ -13,11 +24,12 @@ const CampgroundSchema = new Schema({
         ref: 'User'
     },
     reviews: [
-    {
-        type: Schema.Types.ObjectId,
-        ref: 'Review'
-    }]
-}); 
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
@@ -29,4 +41,4 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
     }
 })
 
-module.exports = mongoose.model('Campground', CampgroundSchema); 
+module.exports = mongoose.model('Campground', CampgroundSchema);
